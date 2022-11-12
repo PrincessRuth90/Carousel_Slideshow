ef2e4958bb64cc60766c889b0c8037608a49d33c
PrincessRuth90 <marcusruth86@gmail.com>
2022-11-11 21:52:56 -0800

    Slideshow

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const swiper = new swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+    //FUNCTION TO GET THE FIRST IMAGE
+let slideIndex = 1;
+showSlides(slideIndex);
+//FUNCTION TO ADD THE NUMBER OF IMAGES CLICKED
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+//FUNCTION TO SLIDE THE IMAGES
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  //LOOPING ACROSS THE IMAGES
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
